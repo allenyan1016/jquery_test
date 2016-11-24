c6a7263f43b45a0cbe0d8281fb47234096ab83a1
yanzhanghong <381936595@qq.com>
2016-11-24 10:51:31 +0800

    jq flash

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
-alert("hello ,hi bitch");
+
+$(function(){
+	var $html = '<img src="marks.jpg" title="marks" id="img1">';
+	$html += '<div id="contro">控制</div>';
+	$html += '<div id="list_test">列表</div>';
+	$html += '<div class="c1">list1</div>';
+	$html += '<div class="c1">list2</div>';
+	$html += '<div class="c1">list3</div>';
+	$html += '<img src="marks.jpg" title="marks" id="img2">';
+	$html += '<div id="contro2">切换</div>';
+	$("#zt").append($html);
+	$("#contro").click(function(){
+		$("#img1").fadeToggle(1000);
+	})
+	$("#contro2").click(function(){
+		$("#img2").toggle(500);
+	})
+	$("#list_test").click(function(){
+		$(".c1").slideToggle();
+	})
+})	
